503770b9b23ef768272f0f412010f56e24ed14f0
zyao <imzhangyao@gmail.com>
2021-12-31 16:27:06 +0800

    Initial commit

---
+++ src/test/java/com/zy/mpdemo/MpdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zy.mpdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MpdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
